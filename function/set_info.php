9df333635ba817aefb5a1a9004e179abbde614a9
Ivan Hanloth <81500828+IvanHanloth@users.noreply.github.com>
2022-07-21 10:35:12 +0800

    v2.2更新

--- function/set_info.php
+++ function/set_info.php
@@ -1,4 +1,5 @@
 <?php
 require "../info.php";
- echo json_encode( array("code"=>"200","filesize"=>$uploadsize,"textsize"=>$textsize,"webname"=>$webname,"header"=>$header,"footer"=>$footer),  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
+header("Access-Control-Allow-Origin:*");
+echo json_encode( array("code"=>"200","filesize"=>$uploadsize,"textsize"=>$textsize,"webname"=>$webname,"header"=>$header,"footer"=>$footer),  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
 ?>
