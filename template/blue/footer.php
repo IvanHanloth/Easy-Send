fe8da2c12788e2d84177c876f25762e4f56f45e5
Ivan Hanloth <81500828+IvanHanloth@users.noreply.github.com>
2022-09-04 23:07:51 +0800

    v2.2.2 更新

--- template/blue/footer.php
+++ template/blue/footer.php
@@ -1,13 +1,22 @@
 <?php /*
 By Ivan Hanloth
 本文件为易传尾部模板文件
-2022/4/3
+2022/9/3
 */
 ?>
 
 <footer>
     <?php echo $footer;  ?>
 </footer>
+<?php
+if(empty($_REQUEST["key"])==false){
+?>
+<script>
+planecss($("#get_panel"));  //校正样式
+$("#get_panel").addClass("layui-show")
+$("#fixed").addClass("layui-show")
+</script>
+<?php } ?>
     <script src="/template/blue/js/main.js"></script>
     <script src="/static/js/function.js"></script>
   </body>
