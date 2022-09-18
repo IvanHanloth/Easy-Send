aebe1f010d016f299b5ac407dfa39c2332642797
Ivan Hanloth <81500828+IvanHanloth@users.noreply.github.com>
2022-09-18 10:26:02 +0800

    Delete template directory

--- template/blue/text.php
+++
@@ -1,8 +0,0 @@
-  <form class="layui-form formbox" action="">
-    <div class="layui-form-item layui-form-text layui-anim layui-anim-upbit" id="text">
-      <textarea name="text" placeholder="请输入内容" class="layui-textarea textarea" lay-verify="text" data-anim="layui-anim-down" id="text-textarea"></textarea>
-    </div>
-    <div class="info" id="text-btn">
-      <button type="submit" class="layui-btn btn layui-anim layui-anim-upbit layui-bg-blue" lay-submit="" lay-filter="save" data-anim="layui-anim-down">立即提交</button></div>
-    <div id="text-info" class="info"></div>
-  </form>
