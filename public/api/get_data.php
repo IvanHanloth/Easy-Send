dd325062190f07b7ca31c784d69c45c3879dbb44
Ivan Hanloth <81500828+IvanHanloth@users.noreply.github.com>
2022-10-02 18:02:29 +0800

    Update get_data.php

--- public/api/get_data.php
+++ public/api/get_data.php
@@ -14,7 +14,7 @@ $key=$key["key"];
 $dbcount=mysqli_query($db,"SELECT count(*) FROM `data` WHERE binary `gkey` = '{$key}'");
 $dbcount=mysqli_fetch_row($dbcount);
 if($dbcount[0]==0) {
-	echo json_encode(array("code"=>"100","tip"=>"不存在此提取码或提取码已过期"),  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
+	echo return_json(array("code"=>"100","tip"=>"不存在此提取码或提取码已过期"));
 } else {
 	$dbinfo=mysqli_query($db,"SELECT * FROM `data` WHERE binary `gkey` = '{$key}'");
 	$dbinfo=mysqli_fetch_assoc($dbinfo);
@@ -24,16 +24,17 @@ if($dbcount[0]==0) {
 	}
 	$tillday=$dbinfo["tillday"];
 	if($times<0) {
-		echo json_encode(array("code"=>"100","tip"=>"不存在此提取码或提取码已过期"),  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
+		echo return_json(array("code"=>"100","tip"=>"不存在此提取码或提取码已过期"));
 	} else {
 	    if($dbinfo["method"]==2){
 	        $rdata=file_get_contents($dbinfo["data"]);
 	    }else{
 	        $rdata=$domain."download/?key=".$dbinfo["gkey"];
 	    }
-		echo json_encode( array("code"=>"200","times"=>$times,"type"=>$dbinfo["type"],"data"=>$rdata,"origin"=>$dbinfo["origin"],"tillday"=>$tillday),  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
-		if($times>0 and $dbinfo["type"]==1) {
+		echo return_json(array("code"=>"200","times"=>$times,"type"=>$dbinfo["type"],"data"=>$rdata,"origin"=>$dbinfo["origin"],"tillday"=>$tillday));
+		if($times>0) {
 			mysqli_query($db,"UPDATE `data` SET `times` = '{$times}' WHERE binary `gkey` = '{$key}'");
+			exit;
 		} else {
 			if($dbinfo["type"]==1 or $dbinfo["method"]==2) {
 				$now=time();
