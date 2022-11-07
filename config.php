477c7daba8b52a61ec3b75c374d95cb65fc54a24
Ivan Hanloth <ivan@hanloth.com>
2022-11-07 22:31:01 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
 $dbconfig=array(
     "host" => "localhost", //数据库服务器
     "port" => 3306, //数据库端口
-    "account" => "new_o5g_top", //数据库用户名
-    "password" => "X6P8JnL2jfXbkhrn", //数据库密码
-    "name" => "new_o5g_top", //数据库名
+    "account" => "", //数据库用户名
+    "password" => "", //数据库密码
+    "name" => "", //数据库名
 );
 ?>
