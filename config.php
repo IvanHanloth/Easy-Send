91b8fc07759f8608243fa349229e23e19375c2c1
Ivan Hanloth <ivan@hanloth.com>
2024-05-05 16:16:57 +0800

    v3.4.2更新

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
 $dbconfig=array(
     "host" => "localhost", //数据库服务器
     "port" => 3306, //数据库端口
-    "account" => "easysend_com", //数据库用户名
-    "password" => "easysend_com", //数据库密码
-    "name" => "easysend_com", //数据库名
+    "account" => "", //数据库用户名
+    "password" => "", //数据库密码
+    "name" => "", //数据库名
 );
 ?>
